2e3645f16d4b95f58ab9f711c40a837d1a92ddc0
benjaug <augenbraun@g.harvard.edu>
2022-06-26 12:35:11 -0400

    incorporate Transition type into linelist methods

--- src/LineLists.jl
+++ src/LineLists.jl
@@ -27,8 +27,8 @@ end
 export Transition
 
 # Function to make a LineList from a ground/excited state pair.
-function makeLineList(ground, excited, p=nothing; TK=10)
-    LineList = zeros(Float64, 0, 2)
+function makeLineList(ground, excited, p=nothing; TK=10, min_I = 1e-10)
+    LineList = Transition[]
  
     QN_g_min = minimum(keys(ground))
     QN_g_max = maximum(keys(ground))
@@ -63,8 +63,10 @@ function makeLineList(ground, excited, p=nothing; TK=10)
                     line_strength = abs2(g_vec' * TDM_mat * e_vec)
                     intensity = line_strength * BF
 
-                    if intensity > 1e-10
-                        LineList = vcat(LineList, [e_val - g_val intensity])
+                    if intensity > min_I
+                        eigl = Eigenstate(g_val, g_vec, ground[QN_g].basis)
+                        eigu = Eigenstate(e_val, e_vec, excited[QN_e].basis)
+                        push!(LineList, Transition(eigl, eigu, intensity, e_val-g_val))
                     end
                 end
             end
@@ -74,25 +76,59 @@ function makeLineList(ground, excited, p=nothing; TK=10)
     return LineList
 end
 
-function label_transitions!(LineList, label_rule)
-    # LineList = Vector{Transition}
-    # label_rule = function that takes a Transition and returns a label
-    for (i,trans) in enumerate(LineList)
-        trans.label = label_rule(trans)
-    end
-end
-export label_transitions!
-
-# This version can make a linelist from a precomputed dictionary of TDM elements between basis states.
-function makeLineList(ground, excited, TDMDict::Dict, p=nothing; TK=10, min_I = 1e-10, return_struct = false)
-    if return_struct == false
-        LineList = zeros(Float64, 0, 2)
-    else
-        LineList = Transition[]
-    end
+# # This version can make a linelist from a precomputed dictionary of TDM elements between basis states.
+# function makeLineList(ground, excited, TDMDict::Dict, p=nothing; TK=10, min_I = 1e-10, return_struct = false)
+#     if return_struct == false
+#         LineList = zeros(Float64, 0, 2)
+#     else
+#         LineList = Transition[]
+#     end
  
-    Elowest = minimum(minimum.([ground[Fg].evals for Fg in keys(ground)]))
+#     Elowest = minimum(minimum.([ground[Fg].evals for Fg in keys(ground)]))
      
+#     for QN_g in keys(ground)
+#         for QN_e = keys(excited)
+#             if typeof(p) <: Vector
+#                 TDM_mat = sum( p[i] * (-1)^(i-2) * TDMDict[QN_e, QN_g, i-2] for i in 1:3) # order basis as [sigma-, pi, sigma+]. i-2 gives [-1,0,+1] to label these.
+#             else    
+#                 TDM_mat = TDMDict[QN_e, QN_g, p]
+#             end 
+#             size_g_state = length(ground[QN_g].basis)
+#             size_e_state = length(excited[QN_e].basis)
+
+#             for m = 1:size_g_state
+#                 g_vec = ground[QN_g].evecs[:,m]
+#                 g_val = ground[QN_g].evals[m]
+#                 for n = 1:size_e_state
+#                     e_vec = excited[QN_e].evecs[:,n]
+#                     e_val = excited[QN_e].evals[n]
+
+#                     BF = exp(-(g_val - Elowest)/(TK) * 0.695) # just set temperaature to 10 K and convert to cm-1. Eventually make this a parameter.
+
+#                     # line_strength = abs(g_vec' * TDM_mat * e_vec)^2
+#                     line_strength = abs2(g_vec' * TDM_mat * e_vec)
+#                     intensity = line_strength * BF
+
+#                     if intensity > min_I
+#                         if return_struct == false
+#                             LineList = vcat(LineList, [e_val - g_val intensity])
+#                         else
+#                             eigl = Eigenstate(g_val, g_vec, ground[QN_g].basis)
+#                             eigu = Eigenstate(e_val, e_vec, excited[QN_e].basis)
+#                             push!(LineList, Transition(eigl, eigu, intensity, e_val-g_val))
+#                         end
+#                     end
+#                 end
+#             end
+#         end
+#     end
+
+#     return LineList
+# end
+# This version can make a linelist from a precomputed dictionary of TDM elements between basis states.
+function makeLineList(ground, excited, TDMDict::Dict, p=nothing; TK=10, min_I = 1e-10)
+    LineList = Transition[]
+    Elowest = minimum(minimum.([ground[Fg].evals for Fg in keys(ground)]))
     for QN_g in keys(ground)
         for QN_e = keys(excited)
             if typeof(p) <: Vector
@@ -117,13 +153,9 @@ function makeLineList(ground, excited, TDMDict::Dict, p=nothing; TK=10, min_I = 
                     intensity = line_strength * BF
 
                     if intensity > min_I
-                        if return_struct == false
-                            LineList = vcat(LineList, [e_val - g_val intensity])
-                        else
-                            eigl = Eigenstate(g_val, g_vec, ground[QN_g].basis)
-                            eigu = Eigenstate(e_val, e_vec, excited[QN_e].basis)
-                            push!(LineList, Transition(eigl, eigu, intensity, e_val-g_val))
-                        end
+                        eigl = Eigenstate(g_val, g_vec, ground[QN_g].basis)
+                        eigu = Eigenstate(e_val, e_vec, excited[QN_e].basis)
+                        push!(LineList, Transition(eigl, eigu, intensity, e_val-g_val))
                     end
                 end
             end
@@ -134,43 +166,79 @@ function makeLineList(ground, excited, TDMDict::Dict, p=nothing; TK=10, min_I = 
 end
 export makeLineList
 
+# # Function to plot a LineList 
+# function plotLineList(LineList, fmin, fmax; gamma = 0.001, nstep=100_000, interactive_labels = false)
+#     step = (fmax-fmin)/(nstep)
+#     freq = zeros(Float64,nstep)
+#     amp = zeros(Float64,nstep)
+#     if interactive_labels == false
+#         @turbo for i = 1:size(LineList,1)
+#             for j = 1:nstep
+#                 offset = j*step
+#                 dif = (offset - (LineList[i,1]-fmin))^2
+#                 freq[j] = offset + fmin
+#                 amp[j] = amp[j] + (gamma/(2*3.141)) * (1/(dif + (gamma^2)/4)) * LineList[i,2]
+#             end
+#         end
+#         return freq, amp
+#     elseif interactive_labels == true
+#         xd = Float64[]
+#         yd = Float64[]
+#         text = String[]    #@turbo for (i,line) = enumerate(LineList)
+#         for (i,line) = enumerate(LineList)
+#             for j = 1:nstep
+#                 offset = j*step
+#                 dif = (offset - (line.frequency-fmin))^2
+#                 freq[j] = offset + fmin
+#                 amp[j] = amp[j] + (gamma/(2*3.141)) * (1/(dif + (gamma^2)/4)) * line.intensity
+#             end
+    
+#             push!(xd,line.frequency)
+#             push!(yd,line.intensity)
+#             push!(text, line.label)
+#         end
+#         ph = plot(xd, yd, linealpha=0, xlabel="Frequency (MHz)", hover = text, frame=:box)
+#         plot!(ph,freq,amp, label="Spectrum", hover = false, legend=false, xlims=(fmin,fmax))
+#         return ph
+#     end
+# end
 # Function to plot a LineList 
-function plotLineList(LineList, fmin, fmax; gamma = 0.001, nstep=100_000, interactive_labels = false)
+function plotLineList(LineList, fmin, fmax; gamma = 0.001, nstep=100_000, make_plot = false)
     step = (fmax-fmin)/(nstep)
     freq = zeros(Float64,nstep)
     amp = zeros(Float64,nstep)
-    if interactive_labels == false
-        @turbo for i = 1:size(LineList,1)
-            for j = 1:nstep
-                offset = j*step
-                dif = (offset - (LineList[i,1]-fmin))^2
-                freq[j] = offset + fmin
-                amp[j] = amp[j] + (gamma/(2*3.141)) * (1/(dif + (gamma^2)/4)) * LineList[i,2]
-            end
+    xd = Float64[]
+    yd = Float64[]
+    text = String[]    
+    for (i,line) = enumerate(LineList)
+        @turbo for j = 1:nstep
+            offset = j*step
+            dif = (offset - (line.frequency-fmin))^2
+            freq[j] = offset + fmin
+            amp[j] = amp[j] + (gamma/(2*3.141)) * (1/(dif + (gamma^2)/4)) * line.intensity
         end
+        push!(xd,line.frequency)
+        push!(yd,line.intensity*(4/(gamma*2*3.141)))
+        push!(text, line.label)
+    end
+    if make_plot == false
         return freq, amp
-    elseif interactive_labels == true
-        xd = Float64[]
-        yd = Float64[]
-        text = String[]    #@turbo for (i,line) = enumerate(LineList)
-        for (i,line) = enumerate(LineList)
-            for j = 1:nstep
-                offset = j*step
-                dif = (offset - (line.frequency-fmin))^2
-                freq[j] = offset + fmin
-                amp[j] = amp[j] + (gamma/(2*3.141)) * (1/(dif + (gamma^2)/4)) * line.intensity
-            end
-    
-            push!(xd,line.frequency)
-            push!(yd,line.intensity)
-            push!(text, line.label)
-        end
+    elseif make_plot == true
         ph = plot(xd, yd, linealpha=0, xlabel="Frequency (MHz)", hover = text, frame=:box)
         plot!(ph,freq,amp, label="Spectrum", hover = false, legend=false, xlims=(fmin,fmax))
         return ph
     end
 end
 export plotLineList
+
+function label_transitions!(LineList, label_rule)
+    # LineList = Vector{Transition}
+    # label_rule = function that takes a Transition and returns a label
+    for (i,trans) in enumerate(LineList)
+        trans.label = label_rule(trans)
+    end
+end
+export label_transitions!
 
 
 # Function to make a LineList from a ground/excited state pair.
